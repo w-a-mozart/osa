96effd06fc05ed9299afd75ed5a38f4686a5b4f4
John Gowing <john.gowing@opb.ca>
2022-07-23 14:00:03 -0400

    catch up on stuff not checked in for a long time (again)

--- osa/osa.registration.js
+++ osa/osa.registration.js
@@ -14,16 +14,7 @@ jQuery(document).ready(function($) {
     updateStudentCount();
   }
 
-  // hack to remove Preludio and Vivace
-  $("[name$='_group_class]'] option[value^='Preludio']").remove();
-  $("[name$='_group_class]'] option[value^='Vivace']").remove();
-  $('[title="Preludio Violin Ensemble"]').hide().prev().hide();
-  $('[title="Vivace Violin Ensemble"]').hide().prev().hide();
-  // hack to remove Reading
-  $("[name$='_kodaly_theory_option_thursday]'] option[value^='Reading']").remove();
-  
-
-  // function to close class option when they are full
+  // function to close class option (e.g. when they are full)
   function closeOption(select_id, opt_value, reason) {
     var sel = $("[id$='"+ select_id + "']");
     sel.focus(function() {
@@ -39,7 +30,7 @@ jQuery(document).ready(function($) {
     opt.attr('disabled', 'disabled');
   }
 
-  // function to close checkbox type option
+  // function to close checkbox option
   function closeCheckBoxOption(cb_id, reason) {
     var cb = $("[id$='"+ cb_id + "']");
     cb.attr('disabled', 'disabled');
@@ -49,46 +40,129 @@ jQuery(document).ready(function($) {
     cb.change();
   }
 
-  // function to close checkbox type option
+  // function to rename checkbox option
   function relabelCheckBoxOption(cb_id, label) {
     var cb = $("[id$='"+ cb_id + "']");
     var lbl = cb.next();
     lbl.html(label);
   }
 
-  /* Covid-hacks for ECM
-  // closeCheckBoxOption('ecm-term-1', 'CANCELLED due to Covid-19');
-  // relabelCheckBoxOption('ecm-term-2', 'Term 2 (6 classes. Jan - March Break)');
-  // closeCheckBoxOption('ecm-term-2', ' <span style="color: red;"> <b>FULL</b></span>');
-  // closeCheckBoxOption('ecm-term-3', 'schedule pending');
-
-  function covidHacksForECM(group) {
-    var saturday_rb = $("[id$='preferred-day-1']");
-    if (group == 'ECM') {
-      saturday_rb.prop("checked", false);
-      saturday_rb.parent().hide();
-      saturday_rb.change();
-      var thursday_rb = $("[id$='preferred-day-2']");
-      thursday_rb.prop("checked", true);
-      thursday_rb.change();
-      
-      $("[id$='ecm-term-1']").attr('disabled', 'disabled');
-      $("[id$='ecm-term-2']").attr('disabled', 'disabled');
-      // $("[id$='ecm-term-3']").attr('disabled', 'disabled');
+  // Hack: the group class and theory classes must be on opposite days
+  $("[name$='preferred_day]']").change(function() {
+    if (this.value === 'Saturday') {
+      $("[id$='preferred-day-kodaly-2']").prop("checked", true);
+      $("label[for$='preferred-day-kodaly-2']").css({'color':'red','font-weight':'bold'});
+      $("label[for$='preferred-day-kodaly-1']").css({'color':'','font-weight':''});
     } else {
-      saturday_rb.parent().show();
+      $("[id$='preferred-day-kodaly-1']").prop("checked", true);
+      $("label[for$='preferred-day-kodaly-1']").css({'color':'red','font-weight':'bold'});
+      $("label[for$='preferred-day-kodaly-2']").css({'color':'','font-weight':''});      
     }
-  }
-
-  var gc_sel = $("[id$='group-class']");
-  covidHacksForECM(gc_sel.val());
-  gc_sel.change(function() {
-    covidHacksForECM(gc_sel.val());
+    $("label[for$='preferred-day-1']").css({'color':'','font-weight':''});
+    $("label[for$='preferred-day-2']").css({'color':'','font-weight':''});      
   });
-  */
+  $("[name$='preferred_day_kodaly]']").change(function() {
+    if (this.value === 'Saturday') {
+      $("[id$='preferred-day-2']").prop("checked", true);
+      $("label[for$='preferred-day-2']").css({'color':'red','font-weight':'bold'});
+      $("label[for$='preferred-day-1']").css({'color':'','font-weight':''});
+    } else {
+      $("[id$='preferred-day-1']").prop("checked", true);
+      $("label[for$='preferred-day-1']").css({'color':'red','font-weight':'bold'});
+      $("label[for$='preferred-day-2']").css({'color':'','font-weight':''});      
+    }
+    $("label[for$='preferred-day-kodaly-1']").css({'color':'','font-weight':''});
+    $("label[for$='preferred-day-kodaly-2']").css({'color':'','font-weight':''});      
+  });
+
+  // Hack:
+  // Cello group class is only on Thursday
+  // ECM1 is only on Thursday, ECM2 is only on Saturday
+  // Beginner Violin is only on Saturday, Kodaly Prep is only on Thursday
+  $(".webform-client-form :input").change(function() {
+    var group = $("[name$='_group_class]']").val();
+    var day_on = '';
+    var day_off = '';
+
+    if (group == 'Cello Group') {
+      day_on = '2';  // Thursday
+      day_off = '1'; // Saturday
+    }
+
+    if (group == 'ECM') {
+      var ecm_level = $("[name$='_ecm_level]']").val();
+      if (!ecm_level) {
+        ecm_level = 'ECM 1';
+        $("[name$='_ecm_level]']").val(ecm_level);
+      }
+
+      if (ecm_level == 'ECM 1') {
+        day_on = '2';  // Thursday
+        day_off = '1'; // Saturday
+      } else { // ECM 2
+        day_on = '1'; // Saturday
+        day_off = '2';  // Thursday
+      }
+      $("[id$='kodaly-theory-option']").val('');
+    }
+
+    if (group == 'Violin Group') {
+      var violin_level = $("[name$='_violin_group]']").val();
+      if (violin_level == 'Beginner (30 min.)') {
+        day_on = '1'; // Saturday
+        day_off = '2';  // Thursday
+      }
+    }
+
+    var kodaly_level = $("[id$='kodaly-theory-option']");
+    if (kodaly_level.val() == 'Kodaly Prep (20 min.)') {
+      if (day_on == '2')  {
+        kodaly_level.addClass('error');
+        alert('You cannot select "Kodaly Prep" since it is on Thursday only');
+        kodaly_level.focus();
+      } else {
+        day_on = '1'; // Saturday
+        day_off = '2';  // Thursday - group off, kodaly on
+      }
+    }
+
+    if (day_on) {
+      // enable and disable the appropriate preferred day
+      $("[id$='preferred-day-" + day_on + "']").prop("checked", true);
+      $("[id$='preferred-day-" + day_off + "']").prop("checked", false).prop('disabled', true);
+      $("label[for$='preferred-day-" + day_on + "']").css({'color':'','font-weight':'', 'text-decoration':''});
+      $("label[for$='preferred-day-" + day_off + "']").css({'color':'','font-weight':'', 'text-decoration':'line-through 3px'});
+      
+      // Kodaly / Theory must be on opposite day
+      $("[id$='preferred-day-kodaly-" + day_off + "']").prop("checked", true);
+      $("[id$='preferred-day-kodaly-" + day_on + "']").prop("checked", false).prop('disabled', true);
+      $("label[for$='preferred-day-kodaly-" + day_off + "']").css({'color':'','font-weight':'', 'text-decoration':''});
+      $("label[for$='preferred-day-kodaly-" + day_on + "']").css({'color':'','font-weight':'', 'text-decoration':'line-through 3px'});
+
+    } else {
+      $("[id$='preferred-day-1']").prop('disabled', false);
+      $("[id$='preferred-day-2']").prop('disabled', false);
+      $("[id$='preferred-day-kodaly-1']").prop('disabled', false);
+      $("[id$='preferred-day-kodaly-2']").prop('disabled', false);
+      $("label[for$='preferred-day-1']").css({'text-decoration':''});
+      $("label[for$='preferred-day-2']").css({'text-decoration':''});
+      $("label[for$='preferred-day-kodaly-1']").css({'text-decoration':''});
+      $("label[for$='preferred-day-kodaly-2']").css({'text-decoration':''});
+    }
+  });
+
+  // add formatting to descriptions
+  $(".description").each(function( i ) {
+    txt = $(this).html();
+    txt = txt.replace("or none of the above", "or <span style=\"font-weight: 900; font-style: italic;\">none of the above</span>");
+    $(this).html(txt);
+  });
+
+  // Hack: these are for term 1 only
+  $("label[for$='enrichment-programs-1']").text('Chamber Music Program - Term 1');
+  $("label[for$='enrichment-programs-3']").text('Creative Ability Development (Musical Improvisation) - Term 1');
 
   // close any full options
   // closeOption('kodaly-theory-option-thursday', 'RCM Theory Levels 3 / 4 / 5 (45 min.)', 'Full');
   // closeOption('kodaly-theory-option-thursday', 'RCM Theory Levels 6 / 7 / 8 (45 min.)', 'Full');
-
 });
--- osa/osa.registration.price.js
+++ osa/osa.registration.price.js
@@ -19,10 +19,6 @@ jQuery(document).ready(function($) {
 
     if (selectedOption.includes('RCM Theory')) {
       optionKey = 'RCM';
-    } else if (selectedOption.includes('Parent Training')) {
-      optionKey = 'Parent_Training';
-    } else if (selectedOption.includes('Reading')) {
-      optionKey = 'Reading';
     } else {
       if (selectedOption.includes('Kodaly')) {
         optionKey = 'Kodaly_';
@@ -37,27 +33,22 @@ jQuery(document).ready(function($) {
   }
 
   function generatePrice() {
-    var memAmount = ((membership) && (Number(membership.end_date.substring(0,4)) >= Number(schoolDates.end.date.substring(0,4)))) ? 0 : 95;
+    var memAmount = ((membership) && (Number(membership.end_date.substring(0,4)) >= Number(schoolDates.end.date.substring(0,4)))) ? 0 : priceTable['Membership'];
     var groupName = '';
     var groupAmount = 0;
     var theoryName = '';
     var theoryAmount = 0;
-    var prevPaidAmount = 0.00;
-    var group = $("[name$='_group_class]']").val();
-    var preferredDay = $("[name*='_preferred_day]']:checked").val();
-    if (preferredDay) { preferredDay = preferredDay.toLowerCase(); }
+    var enhTotal = 0;
 
-    /* hack - Kodaly Only cannot choose Reading */
-    if ((group == 'Kodaly-Theory') && (preferredDay == 'thursday')) {
-      $("[name$='_theory_option_thursday]'] option[value^='Reading']").hide();
-      var tmpStr = '' + $("[name$='_theory_option_thursday]']").val();
-      if (tmpStr.includes('Reading')) {
-        $("[name$='_theory_option_thursday]']").val('');
-      }
-    } else {
-      $("[name$='_theory_option_thursday]'] option[value^='Reading']").show();
-    }
-    /* end of hack */
+    var group = $("[name$='_group_class]']").val();
+    if (!group) { group = 'none'; }
+    var thryPriceKeys = [ group ];
+    var grpPriceKeys = [ group ];
+
+    var preferredDay = $("[name*='_preferred_day]']:checked").val();
+    if (!preferredDay) { preferredDay = ''; }
+    var preferredDayKodaly = $("[name*='_preferred_day_kodaly]']:checked").val();
+    if (!preferredDayKodaly) { preferredDayKodaly = ''; }
 
     /* determine pricing based on selected group and theory options */
 
@@ -77,50 +68,29 @@ jQuery(document).ready(function($) {
 
     /* code for non-ECM */
     } else {
-      if (!group) { group = ''; }
-      var grpPriceKeys = [ group ];
-      var thryPriceKeys = [ group ];
-      var theorySel = "[name$='_theory_option_" + preferredDay + "]']";
 
       /* for Violin or Cello groups, find the level / sub-class */
       if ((group == 'Violin Group') || (group == 'Cello Group')) {
         var level = (group == 'Violin Group') ?
-                    $("[name$='_violin_group_" + preferredDay + "]']").val() :
+                    $("[name$='_violin_group]']").val() :
                     $("[name$='_cello_group]']").val();
         if (level) {
-          groupName = group + ' - ' + level;
+          groupName = group + ' - ' + level + ' - ' + preferredDay;
           grpPriceKeys.push(parseOptionKey(level));
         }
-        /* Cello students can only take theory on Saturday due to scheduling conflicts */
-        if (group == 'Cello Group') { theorySel = "[name$='_theory_option_saturday]']"; }
 
-      /* for advanced Violin use the advance theory options */
-      } else if ((group == 'Preludio') || (group == 'Vivace')) {
-        groupName = group.concat(' Violin Ensemble');
-        theorySel = "[name$='_theory_option_advanced]']";
-
-      /* for chamber groups also use the advance theory options */
-      } else if (group == 'Chamber') {
-        groupName = 'Chamber Music Program';
-        theorySel = "[name$='_theory_option_advanced]']";
-      
-      /* for Kodaly / Theory only, unset the group class settings */
-      } else if (group == 'Kodaly-Theory') {
+      /* otherwise unset the group class settings */
+      } else {
         groupName = '';
         grpPriceKeys = [];
       }
       
-      /* now determine the Kodaly / Theory option */
+      /* determine the Kodaly / Theory option */
+      var theorySel = "[name$='kodaly_theory_option]']";
       theoryName += $(theorySel).val();
       if (theoryName == 'null') theoryName = '';
       thryPriceKeys.push(parseOptionKey(theoryName));
-      if (theoryName.includes('Reading')) {
-        var bundleOption = $("[name$='_theory_option_bundle]']:enabled").val();
-        if (bundleOption) {
-          theoryName += ' + ' + bundleOption;
-          thryPriceKeys.push(parseOptionKey(bundleOption));
-        }
-      }
+      theoryName += ' - ' + preferredDayKodaly;
       
       /* get the price from the price table */
       var price_idx = grpPriceKeys.join('_').replace(/\s|-/g, '_');
@@ -129,18 +99,6 @@ jQuery(document).ready(function($) {
       theoryAmount = isNaN(priceTable[price_idx]) ? 0 : priceTable[price_idx];
     } /* end of non-ECM */
 
-    /* calculate amounts already paid */
-    if (($('[name$="_group_class]"]').val()) && (groupDefaults[_contact_id])) {
-      grpDefault = groupDefaults[_contact_id];
-      key = group.replace(' ', '_');
-      if ((groupAmount > 0) && (grpDefault[key])) {
-        prevPaidAmount += parseFloat(grpDefault[key].prev_paid);
-      }
-      if ((theoryAmount > 0) && (grpDefault['Kodaly-Theory'])) {
-        prevPaidAmount += parseFloat(grpDefault['Kodaly-Theory'].prev_paid);
-      }
-    }
-    
     /* update the prices being displayed */
     if (memAmount > 0) {
       $("#membership-amount").html(`${memAmount}.00`);
@@ -151,11 +109,6 @@ jQuery(document).ready(function($) {
 
     if (groupAmount > 0) {
       groupName = groupName.replace(/ \(.*?\)/g,"");
-      if ((groupAmount <= 40) && (groupName.includes('Early Childhood') || groupName.includes('Chamber'))) {
-        groupName = groupName + " - DEPOSIT";
-      } else {
-        groupName = groupName + " - Term 3";
-      }
       $("#group-name").html(groupName);
       $("#group-amount").html(`${groupAmount}.00`);
       $("#line-item-group").show();
@@ -169,10 +122,8 @@ jQuery(document).ready(function($) {
       }
     }
 
-//    if ((theoryAmount > 0) || (theoryName.includes('Kodaly') && (groupName.includes('Violin Group') || groupName.includes('Cello Group')))) {
-//    if ((theoryAmount > 0) || ((theoryName.length > 0) && (groupName.includes('Violin Group') || groupName.includes('Cello Group') || groupName.includes('Chamber')))) {
     if (theoryAmount > 0) {
-      theoryName = theoryName.replace(/ \(.*?\)/g,"") + " - Term 3";
+      theoryName = theoryName.replace(/ \(.*?\)/g,"");
       $("#theory-name").html(theoryName);
       $("#theory-amount").html(`${theoryAmount}.00`);
       $("#line-item-theory").show();
@@ -180,74 +131,46 @@ jQuery(document).ready(function($) {
       $("#line-item-theory").hide();
     }
 
-    if (prevPaidAmount > 0) {
-      $("#prev-paid-amount").html(prevPaidAmount.toFixed(2));
-      $("#line-item-prev-paid").show();
-    } else {
-      $("#line-item-prev-paid").hide();
-    }
+    $("[name*='enrichment_programs]']").each(function( i ) {
+      ths = $(this);
+      lid = '#line-item-enhance-' + (i+1);
 
-    var totalAmount = memAmount + groupAmount + theoryAmount - prevPaidAmount;
-    $("#total-amount").html(`${totalAmount}.00`);
+      if(ths.prop('checked')) {
+        var name = ths.prop('name');
+        var val  = name.substring(name.lastIndexOf("[") + 1, name.length - 1);
+        enhAmount = isNaN(priceTable[val]) ? 0 : priceTable[val];
+        enhTotal += enhAmount;
+
+        $("#enhance-" + (i+1) + "-name").html(ths.next().text().replace(/ \(.*?\)/g,""));
+        $("#enhance-" + (i+1) + "-amount").html(`${enhAmount}.00`);
+        $(lid).show();
+      } else {
+        $(lid).hide();
+      }
+    });
+
+    // previous paid not used
+    $("#line-item-prev-paid").hide();
+    
+    var totalAmount = memAmount + groupAmount + theoryAmount + enhTotal;
+    $("#total-amount").html(totalAmount.toFixed(2));
   }
 
-//  $("[name$='_custom_205]']").change(function() {
+  // re-calculate prices when something changes
   $(".webform-client-form :input").change(function() {
     generatePrice();
   });
 
-  // function to set default values if the student is already registered
-  function setDefaults(grpDefault) {
-    console.log(grpDefault);
-
-    var grp_class = '';
-    var found = false;
-    var participant;
-    var tmp_element = '';
-
-    // loop through the group class select until find match
-    $('[name$="_group_class]"] option').each(function(index,item) {
-      if (found)
-        return;
-      grp_class = item.value;
-      key = grp_class.replace(' ', '_');
-      if (grpDefault[key]) {
-        found = true;
-        $('[name$="_group_class]"]').val(grp_class).change();
-        participant = grpDefault[key];
-      }
-    });
+  // dynamically build rows in price table for enhancement programs
+  numEnh = $("[name*='enrichment_programs]']").size();
+  theory_line = $("#line-item-theory");
+  last_line = theory_line;
+  $("[name*='enrichment_programs]']").each(function( i ) {
+    new_line = theory_line.clone().prop('id','line-item-enhance-' + (i+1));
+    new_line.html(theory_line.html().replaceAll('theory-', 'enhance-' + (i+1) + '-'));
+    last_line.after(new_line);
+    last_line = new_line;
+  });
   
-    if (!found)
-      return;
-
-    if (participant.custom_104) {
-      $('[name$="_preferred_day]"][value="' + participant.custom_104 + '"]').prop('checked', true).change();
-      tmp_element = participant.custom_104.toLowerCase();
-    }
-
-    if (grp_class == 'ECM') {
-      $('[name$="_ecm_level]"]').val(participant.participant_fee_level.substr(0,5)).change();      
-    } else if (grp_class == 'Violin Group') {
-      $('[name$="_violin_group_'+ tmp_element + ']"]').val(participant.participant_fee_level).change();
-    } else if (grp_class == 'Cello Group') {
-      $('[name$="_cello_group]"]').val(participant.participant_fee_level).change();
-      tmp_element = 'saturday';
-    } else if (grp_class == 'Chamber') {
-      tmp_element = 'advanced';
-    } else if (grp_class == 'Kodaly-Theory') {
-    }
-
-    if (grpDefault['Kodaly-Theory']) {
-      participant = grpDefault['Kodaly-Theory'];
-      $('[name$="_kodaly_theory_option_'+ tmp_element + ']"]').val(participant.participant_fee_level).change();
-    }
-  }
-
-  // if the student has previously registered, and hasn't selected anything, set the defaults to previous registration values
-  if ((!$('[name$="_group_class]"]').val()) && (groupDefaults[_contact_id])) {
-    setDefaults(groupDefaults[_contact_id]);
-  }
-
   generatePrice();
 });
